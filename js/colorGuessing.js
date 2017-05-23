02b79209d8d3180f9698667a536e2e99875bccac
QbanCowboy <qbancowboy@qbancowboy.com>
2017-05-22 19:17:04 -0700

    Almost done with the video series: one last video to go.

--- js/colorGuessing.js
+++ js/colorGuessing.js
@@ -48,7 +48,7 @@ resetBtn.addEventListener("click", function(){
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	messageDisplay.textContent = "";
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 	for (var i=0; i<squares.length; i++){
 		squares[i].style.backgroundColor = colors[i];
 	}
